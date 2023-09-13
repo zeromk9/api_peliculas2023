5393dfcbf159eeb0c858036fdd541b7e0f0f672b
Martin David Corral Castro <zeromarthk@hotmail.com>
2023-09-13 12:00:21 -0600

    c3:card_swiper1

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiper extends StatelessWidget {
@@ -5,10 +6,46 @@ class CardSwiper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Obtiene el tamaño de la pantalla
+    final size = MediaQuery.of(context).size;
+
+    // Devuelve un contenedor con propiedades específicas
     return Container(
-      width: double.infinity, //Agarra el ancho de la pantalla
-      height: 500,
-      color: Colors.red,
+      width: double.infinity, // Ocupa todo el ancho de la pantalla
+      height:
+          size.height * 0.5, // Altura es la mitad de la altura de la pantalla
+      color: Colors.amber, // Color de fondo del contenedor
+
+      // Dentro del contenedor, se utiliza el widget Swiper
+      child: Swiper(
+        itemCount: 10, // Número total de elementos en el Swiper
+        layout: SwiperLayout.STACK, // Diseño de apilamiento para las tarjetas
+        itemWidth: size.width *
+            0.6, // Ancho de cada tarjeta es el 60% del ancho de la pantalla
+        itemHeight: size.height *
+            0.4, // Altura de cada tarjeta es el 40% de la altura de la pantalla
+
+        // Función que construye cada tarjeta
+        itemBuilder: (_, int index) {
+          return GestureDetector(
+            // Detecta gestos, como tocar la tarjeta
+            onTap: () => Navigator.pushNamed(context, 'details',
+                arguments: ''), // Navega a una ruta llamada 'details'
+            child: ClipRRect(
+              // Recorta las esquinas de la tarjeta
+              borderRadius:
+                  BorderRadius.circular(20), // Radio de borde redondeado
+              child: const FadeInImage(
+                // Imagen con efecto de aparición gradual
+                placeholder: AssetImage(
+                    'assets/no-image.jpg'), // Imagen de carga hasta que se carga la real
+                image: NetworkImage(
+                    'https://via.placeholder.com/300x400'), // Imagen real desde una URL
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }

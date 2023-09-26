64a30952a924a6d9f105d986bda2edf1f5dae312
Martin David Corral Castro <zeromarthk@hotmail.com>
2023-09-26 12:07:23 -0600

    prov1:json->class

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:api_peliculas2023/screens/screens.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(const MyApp());
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MoviesProvider(),
+          lazy: false,
+        ),
+      ],
+      child: const MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
